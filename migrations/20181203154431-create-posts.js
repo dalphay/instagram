1ed6df346a3ef4e716595df764d4dda5b44562e9
jackobdiallo@gmail.com <jackobdiallo@gmail.com>
2018-12-04 11:22:37 +0100

    add Foreign Key

--- migrations/20181203154431-create-posts.js
+++ migrations/20181203154431-create-posts.js
@@ -21,7 +21,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id"
+        }
       },
       createdAt: {
         allowNull: false,
